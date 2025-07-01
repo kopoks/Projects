ef00fc219ccb38fc8ef82c418ddea177fc336a07
Kingsley <kopoku042@st.ug.edu.gh>
2025-07-01 21:56:51 +0000

    Update .py

--- .py
+++ .py
@@ -1,3 +1,4 @@
+# Project 1
 # Quiz Game
 
 user = input("What is the fastest land animal?: ")
@@ -203,9 +204,53 @@ if score10 == 55:
     print(f"Outstanding! You got {score10} out of 55")
 elif score10 <= 55:
     print(f"You got {score10} out of 55")
-
-
 # if score1 == 10:
 #     print(f"Outstanding! You got {score1} out of 10")
 # elif score1 <= 55:
 #     print(f"You got {score1} out of 10")
+
+
+
+
+# Project 2
+# Basic Arithmetic Operations
+# Objectve: Build a small program that allows the user to choose from a menu and perform different geometry calculations.
+# Geometry Calculator
+
+import math
+while True:
+    print("Welcome to the Geometry Calculator")
+    print("What would you like to calculate?")
+    print("1. Area of a circle")
+    print("2. Circumference of a circle")
+    print("3. Hypotanus of a triangle")
+    print("4. Exit")
+
+    user = int(input("Enter a number to choose what you want to calculate? "))
+
+    if user < 1 or user > 4:
+        print("Invalid choice, choose again")
+        continue
+    break
+
+
+if user == 1:
+    radius = float(input("What is the radius of the circle? "))
+    area = math.pi * pow(radius, 2)
+    print(f"The area of the circle is {math.ceil(area)} cm^2")
+
+elif user == 2:
+    radius = float(input("What is the radius of the circle? "))
+    circumference = 2 * math.pi * radius
+    print(f"The circumference of the circle is {math.floor(circumference)} cm")
+
+elif user == 3:
+    first_side = float(input("Enter a number for the first side of the triangle: "))
+    second_side = float(input("Enter a number for the second side of the triangle: "))
+    hypotenuse = math.sqrt(pow(first_side, 2) + pow(second_side, 2))
+    print(f"The hypotanus of the triangle is {hypotenuse}")
+
+
+else:
+    print("Thank you for using the geometry calculator")
+
